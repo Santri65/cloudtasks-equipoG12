7ce94b071754bee68d2e35ff956b61812198dfb3
JuanPabloGam <160494228+JuanPabloGam@users.noreply.github.com>
2026-09-12 14:21:04 -0500

    agregando java.script

---
+++ js/supaBaseClient.js
@@ -0,0 +1,2 @@
+const SUPABASE_URL= "https://ynpiszwcyjfoahgzqhyl.supabase.co"
+const SUPABASE_ANON_KEY= "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlucGlzendjeWpmb2FoZ3pxaHlsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkxNDk5NjcsImV4cCI6MjEwNDcyNTk2N30.NKyQWwRnKCJI2pnaX-uwF6B4lgMr3w2U2nmRDctW9Ng"
